3adaf574bd0d889a1e1706798e31da5b90461b4a
Moisés <daw.moisesguerola@gmail.com>
2025-04-11 20:44:52 +0200

    fix: update router method calls to use getRouter() for consistency

--- public/index.php
+++ public/index.php
@@ -8,27 +8,27 @@ require_once '../vendor/autoload.php';
 
 $app = Kernel::bootstrap();
 
-$app->router->get("/test/{param}", function(Request $request) {
+$app->getRouter()->get("/test/{param}", function(Request $request) {
     return Response::json(["result" => $request->getRouteParameters()]);
 });
 
-$app->router->post("/test", function(Request $request) {
+$app->getRouter()->post("/test", function(Request $request) {
     return Response::json(["result" => $request->getData()]);
 });
 
-$app->router->get("/redirect", function(Request $request) {
+$app->getRouter()->get("/redirect", function(Request $request) {
     return Response::redirect("/test/asdf");
 });
 
-$app->router->put('/test', function(Request $request) {
+$app->getRouter()->put('/test', function(Request $request) {
     return "PUT OK";
 });
 
-$app->router->patch('/test', function(Request $request) {
+$app->getRouter()->patch('/test', function(Request $request) {
     return "PATCH OK";
 });
 
-$app->router->delete('/test', function(Request $request) {
+$app->getRouter()->delete('/test', function(Request $request) {
     return "DELETE OK";
 });
 

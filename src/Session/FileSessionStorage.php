db7081384f35b0f19507f5b96975963b2d9cadb0
Moisés Guerola <daw.moisesguerola@gmail.com>
2025-04-24 16:24:40 +0000

    feat: implement set, has, get methods

--- src/Session/FileSessionStorage.php
+++ src/Session/FileSessionStorage.php
@@ -2,12 +2,16 @@
 
 namespace Pickles\Session;
 
-class FileSessionStorage implements SessionStorage {
-    protected string $filesPath = "/pickles/sessions";
+use Constants;
 
+class FileSessionStorage implements SessionStorage
+{
+    protected string $filesPath = "/pickles/sessions";
+    protected array $cachedFiles = [];
     public function start() {}
 
-    public function id(): string {
+    public function id(): string
+    {
         $actualSessionId = $_COOKIE[Constants::FRAMEWORK_SESSION_ID_KEY] ?? null;
         if ($actualSessionId == null) {
             $actualSessionId = $this->generateSessionId();
@@ -17,28 +21,62 @@ class FileSessionStorage implements SessionStorage {
         return $actualSessionId;
     }
 
-    public function get(string $key, $default = null): mixed { return null; }
-    public function set(string $key, mixed $value) { return null; }
-    public function has(string $key): bool { return null; }
-    public function remove(string $key) { return null; }
-    public function destroy(): bool { return null; }
-    public function save(): bool { return null; }
+    public function get(string $key, $default = null): mixed
+    {
+        $session = $this->read();
+        return $session[$key] ?? $default;
+    }
 
-    public function read(string $sessionId = $this->id()): string {
-        $path = $this->filesPath . DIRECTORY_SEPARATOR . $sessionId . ".psctx";
-        if (!file_exists()) {
-            $this->write("");
+    public function set(string $key, mixed $value)
+    {
+        $session = $this->read();
+        $session[$key] = $value;
+    }
+
+    public function has(string $key): bool
+    {
+        $session = $this->read();
+        return isset($session[$key]);
+    }
+
+    public function remove(string $key)
+    {
+        $session = $this->read();
+        unset($session[$key]);
+    }
+
+    public function destroy(): bool
+    {
+        $filePath = $this->getFilePath();
+    }
+    public function save(): bool
+    {
+        return false;
+    }
+
+    public function read(string $sessionId = $this->id()): string
+    {
+        $path = $this->getFilePath($sessionId);
+        if (!file_exists($path)) {
+            $this->write(serialize(""));
         }
 
-        $fileContent = unserialize(file_get_content($path));
+        $fileContent = unserialize(file_get_contents($path));
         return $fileContent;
     }
 
-    public function write(string $content) {
-        file_put_content("{$this->filesPath}/{$this->id()}.psctx", serialize($content));
+    public function write(string $content)
+    {
+        file_put_contents($this->getFilePath(), serialize($content));
     }
 
-    public function generateSessionId(int $num_bytes = 4): string {
+    public function generateSessionId(int $num_bytes = 4): string
+    {
         return bin2hex(openssl_random_pseudo_bytes($num_bytes));
+    }
+
+    public function getFilePath(string $sessionId = $this->id()): string
+    {
+        return $this->filesPath . DIRECTORY_SEPARATOR . $sessionId . ".psctx";
     }
 }
